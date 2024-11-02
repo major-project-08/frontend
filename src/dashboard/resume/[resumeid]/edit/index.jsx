a9bc3c044e54f87f5d7f9613c4ca1112700085b8
Apoorv jain <apoorvjain7222@gmail.com>
2024-11-02 13:32:46 +0530

    dynamic

--- src/dashboard/resume/[resumeid]/edit/index.jsx
+++ src/dashboard/resume/[resumeid]/edit/index.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import FormSection from './../../components/FormSection'
+import ResumePreview from './../../components/ResumePreview'
 function EditResume() {
     const params = useParams()
     useEffect(() => {
         console.log(params.resumeid)
     }, [])
     return (
-        <div>EditResume</div>
+        <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
+            <FormSection />
+            <ResumePreview />
+        </div>
     )
 }
 
---
+++ src/dashboard/resume/components/FormSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FormSection() {
+    return (
+        <div className='bg-primary'>FormSection</div>
+    )
+}
+
+export default FormSection
