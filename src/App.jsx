435932a5a1cbed46c1ff6cc39e80fbb737a0637f
Apoorv jain <apoorvjain7222@gmail.com>
2024-11-16 14:03:01 +0530

    test 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from '@/components/ui/button'
-import { Outlet } from 'react-router-dom'
-import { useUser } from '@clerk/clerk-react'
-import { Navigate } from 'react-router-dom'
-import Header from '@/components/custom/header'
+import Header from '@/components/custom/header.jsx'
 import { Toaster } from '@/components/ui/sonner'
+import { useUser } from '@clerk/clerk-react'
+import { useState } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const { isSignedIn, user ,isLoaded} = useUser()
