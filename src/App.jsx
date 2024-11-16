120a5bd4c8e3af5d599f9964442c24f514dfd653
Apoorv jain <apoorvjain7222@gmail.com>
2024-11-16 14:23:30 +0530

    test 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useUser } from '@clerk/clerk-react'
 import { useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import './App.css'
-import Header from './components/custom/Header'
+import Header from './components/custom/Header.jsx'
 function App() {
   const [count, setCount] = useState(0)
   const { isSignedIn, user ,isLoaded} = useUser()
