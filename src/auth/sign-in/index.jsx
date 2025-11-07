9a13f20ac1e0155b6f789e36f69d344e228aa79b
Apoorv jain <apoorvjain7222@gmail.com>
2025-11-07 23:28:08 +0530

    sign up solved

--- src/auth/sign-in/index.jsx
+++ src/auth/sign-in/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { SignIn, SignUp } from '@clerk/clerk-react'
+import { SignUp } from '@clerk/clerk-react'
 import Header from '@/components/custom/header'
 function SignInPage() {
     return (
         <>
             {/* <Header /> */}
             <div className='flex justify-center items-center my-20'>
-                <SignIn />
                 <SignUp />
             </div>
         </>
